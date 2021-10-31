4e66dfb35f5d4bc478234a0ed60a12f0464616e8
Luiz Tanure <letanure@gmail.com>
2021-10-31 08:10:59 -0300

    feat(generator): add folder option

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -6,27 +6,44 @@ module.exports = (plop) => {
         type: 'input',
         name: 'name',
         message: 'What is your component name?'
+      },
+      {
+        type: 'rawlist',
+        name: 'folder',
+        message: 'What kind of component?',
+        choices: [
+          {
+            key: 'r',
+            name: 'Normal',
+            value: 'components'
+          },
+          {
+            key: 'u',
+            name: 'UI',
+            value: 'components/ui'
+          }
+        ]
       }
     ],
     actions: [
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}/index.tsx',
+        path: '../src/{{folder}}/{{pascalCase name}}/index.tsx',
         templateFile: 'templates/Component.tsx.hbs'
       },
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}/styles.ts',
+        path: '../src/{{folder}}/{{pascalCase name}}/styles.ts',
         templateFile: 'templates/styles.ts.hbs'
       },
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}/stories.tsx',
+        path: '../src/{{folder}}/{{pascalCase name}}/stories.tsx',
         templateFile: 'templates/stories.tsx.hbs'
       },
       {
         type: 'add',
-        path: '../src/components/{{pascalCase name}}/test.tsx',
+        path: '../src/{{folder}}/{{pascalCase name}}/test.tsx',
         templateFile: 'templates/test.tsx.hbs'
       }
     ]
