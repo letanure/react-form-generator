bd32ad873b2d74852bcec8226a7ad4a7d1d9ec11
Luiz Tanure <letanure@gmail.com>
2021-12-02 19:38:33 +0100

    chore: add basic input & form type
    closes #6 #7

---
+++ src/components/ui/types.d.ts
@@ -0,0 +1,57 @@
+type FieldTypes =
+  | 'color'
+  | 'datetime-local'
+  | 'datetime'
+  | 'email'
+  | 'hidden'
+  | 'hidden'
+  | 'month'
+  | 'month'
+  | 'number'
+  | 'password'
+  | 'search'
+  | 'search'
+  | 'tel'
+  | 'text'
+  | 'time'
+  | 'url'
+
+type FieldConfig = {
+  /**teste */
+  label: string
+  name: string
+  type: FieldTypes
+  value: FieldData['value']
+  placeholder?: string
+}
+
+type FormConfig = {
+  fields: FieldConfig[]
+  title?: string
+}
+
+type FieldData = {
+  value: string | number | readonly string[] | undefined
+  changed: boolean
+  touched: boolean
+  valid: boolean
+}
+
+type FieldsValues = {
+  [key: string]: FieldData['value']
+}
+
+/** Information about the state of the form and complete data from each field */
+type FieldsetMeta = {
+  changed: boolean
+  touched: boolean
+  valid: boolean
+  fields: {
+    [key: string]: FieldData
+  }
+}
+
+type FormFieldData = {
+  meta: FieldsetMeta
+  values: FieldsValues
+}
