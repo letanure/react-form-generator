0fafd5928d92321da3790942d8738e2537801129
Luiz Tanure <letanure@gmail.com>
2021-12-03 02:10:50 +0100

    fix: font family

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -17,7 +17,8 @@ const GlobalStyles = createGlobalStyle`
        font-size: 62.5%;
      }
      body {
-       font-family: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif";
+       font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+         Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
        font-size: 1.6rem;
        background-color: #f5f5f5;
      }
