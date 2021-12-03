2af89ee4d949141b060b19ecfbab0898f2eea602
Luiz Tanure <letanure@gmail.com>
2021-12-03 03:49:00 +0100

    feat: add demo Form

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,18 +1,130 @@
 import * as S from './styles'
+import Form from 'components/ui/Form'
+import { useState } from 'react'
+import CodeBlock from 'components/ui/CodeBlock'
 
-interface Props {
-  /**
-   * Main heading title
-   */
-  title?: string
-  description?: string
+const formConfig: FormConfig = {
+  title: 'My form',
+  fields: [
+    {
+      label: 'Placeholder text',
+      placeholder: 'Placeholder test',
+      name: 'placeholder',
+      type: 'text',
+      value: ''
+    },
+    {
+      label: 'Color',
+      name: 'color',
+      type: 'color',
+      value: '#ff0000'
+    },
+    {
+      label: 'Email',
+      name: 'email',
+      type: 'email',
+      value: 'aa@bb.cc'
+    },
+    {
+      label: 'Number',
+      name: 'number',
+      type: 'number',
+      value: '1'
+    },
+    {
+      label: 'Password',
+      name: 'password',
+      type: 'password',
+      value: '123456'
+    },
+    {
+      label: 'tel',
+      name: 'tel',
+      type: 'tel',
+      value: '111222333'
+    },
+    {
+      label: 'Text',
+      name: 'text',
+      type: 'text',
+      value: 'my text'
+    },
+    {
+      label: 'Time',
+      name: 'time',
+      type: 'time',
+      value: '12:00'
+    },
+    {
+      label: 'URL',
+      name: 'url',
+      type: 'url',
+      value: 'https://www.google.com'
+    },
+    {
+      label: 'Datetime',
+      name: 'datetime',
+      type: 'datetime-local',
+      value: '2020-01-01T12:00'
+    },
+    {
+      label: 'Hidden',
+      name: 'hidden',
+      type: 'hidden',
+      value: '9'
+    },
+    {
+      label: 'Month',
+      name: 'month',
+      type: 'month',
+      value: '2020-01'
+    },
+    {
+      label: 'Search',
+      name: 'search',
+      type: 'search',
+      value: 'my search'
+    }
+  ]
 }
 
-const Main = ({ title = 'React Form Generator' }: Props) => (
-  <S.Wrapper>
-    <S.Logo src="/img/icon-512.png" alt="Atom image." />
-    <S.Title>{title}</S.Title>
-  </S.Wrapper>
-)
+const Main = () => {
+  const [formData, setFormData] = useState<FormFieldData>()
+  const [formMeta, setFormMeta] = useState<FieldsetMeta>()
+
+  const handleOnChange = (
+    formValues: FormFieldData,
+    formMeta: FieldsetMeta
+  ) => {
+    setFormData(formValues)
+    setFormMeta(formMeta)
+  }
+
+  const handleSubmit = (formValues: FieldsValues, formMeta: FieldsetMeta) => {
+    console.log('handleSubmit', formValues, formMeta)
+  }
+  return (
+    <S.Wrapper>
+      {!!formConfig.fields && (
+        <>
+          <S.FormArea>
+            <Form
+              {...formConfig}
+              onChange={handleOnChange}
+              onSubmit={handleSubmit}
+            />
+          </S.FormArea>
+
+          <S.CodeArea>
+            <h3>Data</h3>
+            <CodeBlock codeString={formData} />
+            <h3>Meta</h3>
+            <CodeBlock codeString={formMeta} />
+          </S.CodeArea>
+        </>
+      )}
+    </S.Wrapper>
+  )
+}
 
 export default Main
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,22 +1,32 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main`
-  background-color: #fafafa;
-  width: 100%;
+  background-color: #ffffff;
+  max-width: 800px;
+  margin: 15px auto;
+  box-shadow: 0 1px 8px rgb(0 0 0 / 8%);
   height: 100%;
   padding: 3rem;
-  text-align: center;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  width: 70%;
 `
 
-export const Logo = styled.img`
-  width: 25rem;
-  margin-bottom: 2rem;
+export const FormArea = styled.div`
+  min-width: 400px;
+  padding-right: 30px;
+`
+export const CodeArea = styled.div`
+  position: sticky;
+  top: 30px;
+  border: 2px solid red;
 `
 
-export const Title = styled.h1`
-  font-size: 2.5rem;
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
 `
