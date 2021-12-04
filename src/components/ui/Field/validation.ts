192570a9c22570a9f321b7c36941ee0e957ed43b
Luiz Tanure <letanure@gmail.com>
2021-12-05 00:49:42 +0100

    refactor: isvalid based on nr errors

--- src/components/ui/Field/validation.ts
+++ src/components/ui/Field/validation.ts
@@ -6,38 +6,29 @@ const runValidation = (
   ruleConfig: RuleConfig,
   value: unknown,
   label: string
-): [boolean, string[]] => {
-  const schema: ValidationSchema = {
+): string[] => {
+  const check = validator.compile({
     $$root: true,
     ...ruleConfig
-  }
-  const check = validator.compile(schema)
+  })
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const result: any = check(value)
-  if (result === true) return [true, []]
-  const errors: string[] = result.map(
-    (item: ValidationError) =>
-      ruleConfig.message ||
-      (item && item.message && item.message.replace("''", label))
+  return (
+    (result === true && []) ||
+    result.map(
+      ({ message }: ValidationError) =>
+        ruleConfig.message || (message && message.replace("''", label))
+    )
   )
-  return [false, errors]
 }
 
 export const runValidations = (
   validate: RuleConfig[],
   value: unknown,
-  label: string
-) =>
+  label: string | undefined = ''
+): string[] =>
   validate
     .map((ruleConfig: ValidationSchema) =>
       runValidation(ruleConfig, value, label)
     )
-    .reduce(
-      (acc: [boolean, string[]], [isValid, errors]: [boolean, string[]]) => {
-        return [
-          (acc[0] || isValid) && acc[0] && isValid,
-          [...acc[1], ...errors]
-        ]
-      },
-      [true, []]
-    )
+    .flat()
