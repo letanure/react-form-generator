f04cb22de537ba91a57781b3f10ca7210223ee8a
Luiz Tanure <letanure@gmail.com>
2021-12-12 12:35:30 +0100

    refactor: remove storybook actions
    included on addon essentials

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-actions'],
+  addons: ['@storybook/addon-essentials'],
   typescript: { reactDocgen: false },
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
