0b5ada483695455d5941aabd4f4e26f702a2c8a9
Luiz Tanure <letanure@gmail.com>
2021-12-15 21:53:34 +0100

    feat: load field styles globally

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
+import 'styles/form.sass'
 
 function App({ Component, pageProps }: AppProps) {
   return (
